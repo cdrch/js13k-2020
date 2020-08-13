9267eac8fdbdab715a4bc665a404828b39a741fc
Carter David Henning <carterdavidhenning@gmail.com>
2020-08-13 17:50:06 -0400

    Add generation of random one-color tile textures

--- game.js
+++ game.js
@@ -52,6 +52,8 @@ function setupGame() {
     // map.heigh = MAP_HEIGHT;
     createMap(map.grid, MAP_WIDTH, MAP_HEIGHT);
 
+    createTexture(320, 320);
+
     g.state = playGame;
 }
 
@@ -86,10 +88,42 @@ function createMap(grid, width, height) {
 }
 
 function tileLookup(i) {
-    console.log(i);
     switch(i) {
     case 0:
-        return g.rectangle(32, 32, "#011D80");
+        let newImage = {};
+        newImage.name = "test.png";
+        let a = {};
+        a = new Image();
+        a.src = createTexture(32, 32);
+        g.assets[newImage.name] = {
+            source: a,
+            frame: {
+                x: 0,
+                y: 0,
+                w: 32,
+                h: 32
+            }
+        };
+        
+        /*
+        image.name = source;
+        self[image.name] = {
+            //If you just want the file name and the extension, you can
+            //get it like this:
+            //image.name = source.split("/").pop();
+            //Assign the image as a property of the assets object so
+            //we can access it like this: `assets["images/imageName.png"]`.
+            source: image,
+            frame: {
+                x: 0,
+                y: 0,
+                w: image.width,
+                h: image.height
+            }
+        };*/
+        console.log(g.assets["test.png"]);
+        return g.sprite("test.png");
+        //return g.rectangle(32, 32, "#011D80");
     case 1:
         return g.rectangle(32, 32, "#5771CF");
     case 2:
@@ -101,4 +135,81 @@ function tileLookup(i) {
     default:
         return g.rectangle(32, 32, "#FFFFFF");
     }
+}
+
+function createColor() {
+    //saturation is the whole color spectrum
+    const h = Math.floor(g.randomFloat(0,1) * 360);
+    //saturation goes from 40 to 100, it avoids greyish colors
+    const s = ((g.randomFloat(0,1) * 60) + 40) + "%";
+    //lightness can be anything from 0 to 100, but probabilities are a bell curve around 50%
+    const l = ((g.randomFloat(0,1) + g.randomFloat(0,1) + g.randomFloat(0,1) + g.randomFloat(0,1)) * 25) + "%";
+    //return "black";
+    return "hsl(" + h + "," + s + "," + l + ")";
+}
+
+function createTexture(width, height) {
+    const opts = {};
+
+    opts.seed = opts.seed || Math.floor((Math.random()*Math.pow(10,16))).toString(16);
+
+    opts.size = opts.size || 32;
+    opts.scale = opts.scale || 32;
+    opts.color = opts.color || createColor();
+    opts.bgcolor = opts.bgcolor || createColor();
+    opts.spotcolor = opts.spotcolor || createColor();
+
+
+
+
+    const dataWidth = Math.ceil(width / 2);
+    const mirrorWidth = width - dataWidth;
+    
+    const data = [];
+    for(let y = 0; y < height; y++) {
+        let row = [];
+        for(let x = 0; x < dataWidth; x++) {
+            // this makes foreground and background color to have a 43% (1/2.3) probability
+            // spot color has 13% chance
+            row[x] = Math.floor(g.randomFloat(0,1)*2.3);
+        }
+        const r = row.slice(0, mirrorWidth);
+        r.reverse();
+        row = row.concat(r);
+
+        for(let i = 0; i < row.length; i++) {
+            data.push(row[i]);
+        }
+    }
+
+    let imageData = data;
+
+    let canvas = document.createElement("canvas");
+
+    width = Math.sqrt(imageData.length);
+
+    canvas.width = canvas.height = opts.size * opts.scale;
+
+    const cc = canvas.getContext("2d");
+    cc.fillStyle = opts.bgcolor;
+    cc.fillRect(0, 0, canvas.width, canvas.height);
+    cc.fillStyle = opts.color;
+
+    for(let i = 0; i < imageData.length; i++) {
+
+        // if data is 0, leave the background
+        if(imageData[i]) {
+            const row = Math.floor(i / width);
+            const col = i % width;
+
+            // if data is 2, choose spot color, if 1 choose foreground
+            cc.fillStyle = (imageData[i] == 1) ? opts.color : opts.spotcolor;
+
+            cc.fillRect(col * opts.scale, row * opts.scale, opts.scale, opts.scale);
+        }
+    }
+
+    //console.log(cc.getImageData(0, 0, width, height));
+
+    return canvas.toDataURL("image/png");
 }
